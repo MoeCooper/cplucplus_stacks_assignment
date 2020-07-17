b30b8fcba79ff0fd78b8cb7887e473ade6b0a171
Maurice <42690947+MoeCooper@users.noreply.github.com>
2020-07-17 16:14:36 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <string>
 
+//class called Stack
 class Stack {
+    // making everything public
 public:
     std::string pageTitle;
     std::string pageDescription;
@@ -11,45 +13,14 @@ public:
 
     //pointer to the array on the heap, created dynamically at runtime
     std::string *stringArrayPointer;
+    // size for dynamic memory allocation at runtime
     int size;
+    // top pointer is default to -1 because there would be nothing in the stack
     int topPointer = -1;
+    // annotates the current page the browser is looking at
     int currentPage;
 
-    //Stack(){
-    //    topPointer = -1;
-    //    for(int i = 0, i < size-1; i++){
-
-    //    }
-    //}
-
-    //void setPageTitle(std::string pagetitle){
-    //    pageTitle = pagetitle;
-    //}
-    //std::string getPageTitle(){
-    //    return pageTitle;
-    //}
-
-    //void setPageDescription(std::string pagedescription){
-    //    pageDescription = pagedescription;
-    //}
-    //std::string getPageDescription(){
-    //    return pageDescription;
-    //}
-
-    //void setPageURL(std::string pageurl){
-    //    pageURL = pageurl;
-    //}
-    //std::string getPageURL(){
-    //    return pageURL;
-    //}
-
-    //void setDateAccessed(std::string dateaccessed){
-    //    dateAccessed = dateaccessed;
-    //}
-    //std::string getDateAccessed(){
-    //    return DateAccessed;
-    //}
-
+    // display method to view everything in stack. After sometime, I decided that overloading the << operator to display the object browser directly would suffice. I don't think I would need to loop through the object attributes if I can overload and display the whole object directly??
     void display(Stack browser){
         std::cout << "All values in the stack are " << std::endl;
         for(int i = browser.size; i >=0; i--){
@@ -57,6 +28,7 @@ public:
         }
     }
 
+    // pop function modifies the stack, so we are taking a pointer to reference it when we call it in main
     std::string pop(Stack *browser){
         // variable to setup to show that there is nothing to delete
         std::string noVal = "";
@@ -64,7 +36,9 @@ public:
         if(browser->topPointer == -1){
             std::cout << "The stack is empty" << std::endl;
         } else {
+            // sets noVal to the value of what is in browsers toppointer
             noVal = browser->stringArrayPointer[browser->topPointer];
+            // decrements top pointer in stack
             browser->topPointer--;
         }
         // if nothing is deleted, it will return -1 to show that there is nothing to delete
@@ -72,8 +46,8 @@ public:
     }
 
     // no pointer to browser because I am not modifying the browser. If I were to modify it, then I would need *.
-    int peek(Stack browser, int position){
-        int x = -1;
+    std::string peek(Stack browser, int position){
+        std::string x = "";
         //top - position + 1
         if(browser.topPointer-position + 1 < 0){
             std::cout << "Invalid position" << std::endl;
@@ -84,7 +58,8 @@ public:
         return x;
     }
 
-    void isEmpty(Stack *browser){
+    // simple method to check if the stack is empty or not. Pass by value because no modification to browser is being made.
+    void isEmpty(Stack browser){
         if(browser->topPointer == -1){
             std::cout << "The Stack is empty." << std::endl;
         }
@@ -93,7 +68,8 @@ public:
         }
     }
 
-    void isFull(Stack *browser){
+    // simple method to check if the stack if fill or not. Pass by value because no modification to browser is being made.
+    void isFull(Stack browser){
         if(browser->topPointer == browser->size-1){
             std::cout << "The Stack is full." << std::endl;
         }
@@ -103,10 +79,11 @@ public:
     }
 
     // I need to be able to create website information. Perhaps constructor or can I pass in ... the class with the attributes only??
-    std::string push(Stack *browser, std::string pagetitle, std::string pagedescription, std::string pageurl, std::string dateaccessed){
+    void push(Stack *browser, std::string pagetitle, std::string pagedescription, std::string pageurl, std::string dateaccessed){
         if(browser->topPointer == browser->size-1){
             std::cout << "Stack overflow" << std::endl;
         } else {
+            // here is where I had a hard time. I wasn't sure if I could pass all the values here directly, or if I needed a constructor to do so??
             browser->topPointer++;
             browser->stringArrayPointer[browser->topPointer] = pagetitle;
             browser->stringArrayPointer[browser->topPointer] = pagedescription;
@@ -115,7 +92,8 @@ public:
         }
     }
 
-    std::string goBackPage(Stack *browser){
+    // method I created to move the current pointer down in the stack if user selected "go back" in main menu.
+    void goBackPage(Stack *browser){
         if(browser->currentPage == -1){
             std::cout << "You cannot go back any further" << std::endl;
         } else {
@@ -123,12 +101,21 @@ public:
         }
     }
 
-    std::string goForwardPage(Stack *browser){
-        if(browser->currentPage == stringArrayPointer->size-1){
+    // method I created to move the current pointer up in the stack if user selected "go forward" in main menu.
+    void goForwardPage(Stack *browser){
+        if(browser->currentPage == browser->size-1){
             std::cout << "You cannot go forward any further" << std::endl;
         } else {
             currentPage++;
         }
+    }
+
+    //overloading << operator to directly display information in browser object with std::cout stream
+    std::ostream &operator<<(std::string &stream, const Stack &browse){
+        // displays each attribute of Stack class
+        stream << browse.pageTitle << " " << browse.pageDescription << " " << browse.pageURL << " " << browse.dateAccessed << std::endl;
+        // return the overloaded << stream
+        return stream;
     }
 
 };
@@ -136,10 +123,10 @@ public:
 
 int main(){
     Stack browser;
-    int option, value;
+    int option, value, size;
     std::string pageT, pageD, pageU, dateA;
     std::cout << "How many web pages do you want to insert in the stack?" << "\n";
-    std::cin >> browser.size;
+    std::cin >> size;
     browser.stringArrayPointer = new int[browser.size];
 
 
@@ -160,16 +147,16 @@ int main(){
             browser.goForwardPage(&browser);
             break;
         case 3:
-            browser.display(browser);
+            std::cout << &browser;
             break;
         case 4:
             std::cout << "Enter a page title, page description, page URL, and date accessed: " << std::endl;
             std::getline(std::cin, pageT);
             std::getline(std::cin, pageD);
-            std::cin >> pageU;
+            std::getline(std::cin, pageU);
             std::getline(std::cin, dateA);
 
-            browser.push(&value, pageT, pageD, pageU, dateA);
+            browser.push(&browser, pageT, pageD, pageU, dateA);
         case 5:
             std::cout << "You have deleted " << browser.pop(&browser) << std::endl;
             break;
